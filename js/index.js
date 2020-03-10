754dab009e9d402ea8ff132066a069ee547afe83
xxeebbk <60412445+xxeebbk@users.noreply.github.com>
2020-03-10 09:06:37 -0230

    Added JavaScript
    Started with JavaScript and added a new folder

---
+++ js/index.js
@@ -0,0 +1,27 @@
+console.log("Hello out there!");
+
+let firstName = "Martin";
+let lastName = "O'Driscoll";
+let myAge = "39";
+
+console.log(firstName, lastName, myAge);
+
+let mySkills = ["HTML", "CSS", "JavaScript"];
+
+mySkills [0] = "Windows 10";
+mySkills [3] = "Android User";
+
+console.log(mySkills);
+
+let name = {
+    first: firstName,
+    last: lastName,
+    age: myAge,
+    skills: mySkills
+}
+
+console.log(name);
+console.log(name.last);
+
+name.nickname = "Tan";
+name.skills [3] = "Fixer";
